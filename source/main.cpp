e11e10c1ab4932b04dd956b26e72ef49dd80750f
Gerardo Rodriguez <gerardo.espinoza@nxp.com>
2019-10-11 10:37:19 -0500

    Add DAPLink validation code for microbit v1.43.4 boards with correct TX and RX pins.

--- source/main.cpp
+++ source/main.cpp
@@ -1,36 +1,121 @@
 #include "mbed.h"
+#include <stdio.h>
 
-DigitalOut rows[5] = {
-    DigitalOut(ROW_1, 0),
-    DigitalOut(ROW_2, 0),
-    DigitalOut(ROW_3, 0),
-    DigitalOut(ROW_4, 0),
-    DigitalOut(ROW_5, 0)
+RawSerial pc(P0_6, P1_8);
+
+class ByteBuffer
+{
+private:
+    uint32_t head;
+    uint32_t tail;
+    const size_t size;
+    uint8_t* buffer;
+public:
+    ByteBuffer(size_t buffer_size): size(buffer_size) {
+        head = 0;
+        tail = 0;
+        buffer = new uint8_t[buffer_size];
+    }
+
+    ~ByteBuffer() {
+        delete[] buffer;
+    }
+
+    bool empty() {
+        return head == tail;
+    }
+
+    bool full() {
+        uint32_t new_tail = tail + 1;
+        if (new_tail >= size) {
+            new_tail = new_tail - size;
+        }
+        return head == new_tail;
+    }
+
+    void enqueue(uint8_t data) {
+        if (full()) {
+            error("Queue full\n");
+        }
+        buffer[tail] = data;
+        tail++;
+        if (tail >= size) {
+            tail -= size;
+        }
+    }
+
+    uint8_t dequeue() {
+        uint8_t data;
+        if (empty()) {
+            error("Queue empty\n");
+        }
+        data = buffer[head];
+        head++;
+        if (head >= size) {
+            head = head - size;
+        }
+        return data;
+    }
+
 };
-DigitalOut cols[5] = {
-    DigitalOut(COL_1, 1),
-    DigitalOut(COL_2, 1),
-    DigitalOut(COL_3, 1),
-    DigitalOut(COL_4, 1),
-    DigitalOut(COL_5, 1)
-};
 
-int main(void) {
-    //DigitalOut led(LED1, 0);
+int main()
+{
+    uint32_t baud;
+    uint32_t count;
+    uint32_t index;
+    uint32_t val;
+    uint8_t str[64];
+    ByteBuffer buf(1024);
+    count = 0;
+    index = 0;
 
-    while (true) {
-        for (int i = 0; i < 5; i++) {
-            rows[i] = 1;
-            int prev_i = i - 1;
-            if (prev_i < 0) prev_i = 4;
-            rows[prev_i] = 0;
-            for (int j = 0; j < 5; j++) {
-                cols[j] = 0;
-                int prev_j = j - 1;
-                if (prev_j < 0) prev_j = 4;
-                cols[prev_j] = 1;
-                wait(0.25);
-                //led = !led;
+    pc.baud(115200);
+    pc.printf("{init}");
+    while (1) {
+
+        // Enqueue data as it arrives
+        while (pc.readable() && !buf.full()) {
+            val = pc.getc();
+            buf.enqueue(val);
+        }
+
+        // Process and send data
+        if (!buf.empty() && pc.writeable()) {
+            val = buf.dequeue();
+            // Check for overflow. Leave space for
+            // a null terminating character
+            if (index >= sizeof(str) - 1) {
+                index = 0;
+            }
+
+            // Check for start of frame
+            if ('{' == val) {
+                index = 0;
+            }
+
+            // Check for end of frame
+            str[index] = val;
+            index++;
+
+            // Check for end of frame
+            if ('}' == val && index > 0) {
+                str[index] = 0;
+                count = sscanf((char*)str, "{baud:%i}", &baud);
+            }
+
+            // Echo back character
+            pc.putc(val);
+
+            // Set baud if there is a valid command
+            if (count == 1) {
+                wait(0.01f);
+                pc.baud(baud);
+                // Make sure pc has enough time
+                // LCP11u35 requires ~0.1us while K20D requires ~0.01us
+                wait(0.1f);
+                pc.printf("{change}");
+                count = 0;
             }
         }
     }
