6a310c6e13692dc74172e058e94d3bf4c24a9620
Gerardo Rodriguez <gerardo.espinoza@nxp.com>
2019-10-11 20:54:52 -0500

    Add visual feedback in the LED matrix for every transmitted character.

--- source/main.cpp
+++ source/main.cpp
@@ -59,6 +59,21 @@ public:
 
 };
 
+DigitalOut rows[5] = {
+    DigitalOut(ROW_1, 0),
+    DigitalOut(ROW_2, 0),
+    DigitalOut(ROW_3, 0),
+    DigitalOut(ROW_4, 0),
+    DigitalOut(ROW_5, 0)
+};
+DigitalOut cols[5] = {
+    DigitalOut(COL_1, 1),
+    DigitalOut(COL_2, 1),
+    DigitalOut(COL_3, 1),
+    DigitalOut(COL_4, 1),
+    DigitalOut(COL_5, 1)
+};
+
 int main()
 {
     uint32_t baud;
@@ -69,6 +84,12 @@ int main()
     ByteBuffer buf(1024);
     count = 0;
     index = 0;
+    
+    int i = 0;
+    int j = 0;
+    
+    rows[0] = 1;
+    cols[0] = 0;
 
     pc.baud(115200);
     pc.printf("{init}");
@@ -106,6 +127,19 @@ int main()
 
             // Echo back character
             pc.putc(val);
+            
+            if (i == 5) i = 0;
+            rows[i] = 1;
+            int prev_i = i - 1;
+            if (prev_i < 0) prev_i = 4;
+            rows[prev_i] = 0;
+            if (j == 5) j = 0;
+            cols[j] = 0;
+            int prev_j = j - 1;
+            if (prev_j < 0) prev_j = 4;
+            cols[prev_j] = 1;
+            j++;
+            if (j == 5) i++;
 
             // Set baud if there is a valid command
             if (count == 1) {
